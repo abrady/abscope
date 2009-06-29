74acb8f797d57b2c588123b38b504de0cd5dfa37
unknown <abrady@.paragon.crypticstudios.com>
2009-06-29 09:48:25 -0700

    update logging for scan_src.bat

--- scan_src.bat
+++ scan_src.bat
@@ -1,3 +1,6 @@
 pushd c:\src
-c:\abs\abscope\abscope.exe -R . -Dt -E AutoGen -E 3rdparty > c:\abs\abscope\scan_src_result.txt
+echo "----------" >> c:\abs\abscope\scan_src_result.txt
+date /t >> c:\abs\abscope\scan_src_result.txt
+time /t >> c:\abs\abscope\scan_src_result.txt
+c:\abs\abscope\abscope.exe -R . -Dt -E AutoGen -E 3rdparty >> c:\abs\abscope\scan_src_result.txt
 popd
